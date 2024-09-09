019aed35649d25f1cdc7d77af6b39e5076ce9d33
vargabyan <vardan.gabrielyan.it@gmail.com>
2024-09-09 16:58:00 +0500

    #1

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>pnk-region</title>
+</head>
+<body>
+
+<section class="bottom-menu">
+    <div class="bottom-menu-link-wrapper">
+        <a class="bottom-menu_link icon-tel" href="tel:8 800 222 02 55"><span>8 800 222 02 55</span></a>
+        <a class="bottom-menu_link icon-mail" href="mailto:pokupka@опора-москвы.рф"><span>pokupka@опора-москвы.рф</span></a>
+        <a class="bottom-menu_link icon-whatsapp" href=""></a>
+    </div>
+    <button class="bottom-menu_ntm-request-kp">Запрос КП</button>
+</section>
+
+</body>
+</html>
